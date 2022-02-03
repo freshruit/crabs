04b989f7408425d79a183a393f97584ae20f46bd
Admin <chains1@yandex.ru>
2022-02-03 18:34:31 +0500

    1 классная работа

--- main.py
+++ main.py
@@ -3,21 +3,40 @@ import os
 import sys
 
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget
+from UI import Ui_Form
 
 SCREEN_SIZE = [600, 450]
 
 
-class Map(QWidget):
+class Map(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        self.getImage()
         self.initUI()
 
+    def initUI(self):
+        self.setupUi(self)
+        self.show_map_button.clicked.connect(self.show_map)
+
+    # Вывод изображения карты в зависимости от введённых координат
+    def show_map(self):
+        self.get_coords()
+        self.getImage()
+
+        self.pixmap = QPixmap(self.map_file)
+        self.map_label.setPixmap(self.pixmap)
+
+    # Получение введённых пользователем координат и масштаба
+    def get_coords(self):
+        self.longitude = self.longitude_edit.text().strip()
+        self.width = self.width_edit.text().strip()
+        self.scale = int(self.scale_edit.text().strip()) * 100
+
+    # Получение изображения по запросу
     def getImage(self):
-        longitude = 55.107927
-        width = 51.77721
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={longitude},{width}8&spn=0.002,0.002&l=map"
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll" \
+                      f"={self.longitude},{self.width}8&spn" \
+                      f"={1 / self.scale},{1 / self.scale}&l=map"
         response = requests.get(map_request)
         if not response:
             print("Ошибка выполнения запроса:")
@@ -30,24 +49,10 @@ class Map(QWidget):
         with open(self.map_file, "wb") as file:
             file.write(response.content)
 
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        # Изображение
-        self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(*SCREEN_SIZE)
-        self.image.setPixmap(self.pixmap)
-
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
 
-    # Вывод изображения в зависимости от введённых координат
-    def show_image(self):
-        pass
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
