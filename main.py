bdbd8ba873282803348c992bcadb1624e3517d08
Admin <chains1@yandex.ru>
2022-02-04 00:18:57 +0500

    Классная работа 2 доработанная

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
-import os
 import sys
+import os
 
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import Qt
 from UI import Ui_Form
 
 SCREEN_SIZE = [600, 450]
@@ -16,11 +17,13 @@ class Map(QWidget, Ui_Form):
 
     def initUI(self):
         self.setupUi(self)
+
         self.show_map_button.clicked.connect(self.show_map)
 
     # Вывод изображения карты в зависимости от введённых координат
     def show_map(self):
-        self.get_coords()
+        if self.sender():
+            self.get_coords()
         self.getImage()
 
         self.pixmap = QPixmap(self.map_file)
@@ -28,15 +31,15 @@ class Map(QWidget, Ui_Form):
 
     # Получение введённых пользователем координат и масштаба
     def get_coords(self):
-        self.longitude = self.longitude_edit.text().strip()
         self.width = self.width_edit.text().strip()
-        self.scale = int(self.scale_edit.text().strip()) * 100
+        self.longitude = self.longitude_edit.text().strip()
+        self.scale = int(self.scale_edit.text().strip())
 
     # Получение изображения по запросу
     def getImage(self):
         map_request = f"http://static-maps.yandex.ru/1.x/?ll" \
                       f"={self.longitude},{self.width}8&spn" \
-                      f"={1 / self.scale},{1 / self.scale}&l=map"
+                      f"={1 / (self.scale * 100)},{1 / (self.scale * 100)}&l=map"
         response = requests.get(map_request)
         if not response:
             print("Ошибка выполнения запроса:")
@@ -48,6 +51,17 @@ class Map(QWidget, Ui_Form):
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
+
+    # Обработка клавиатуры
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            if self.scale < 7:
+                self.scale += 1
+                self.show_map()
+        if event.key() == Qt.Key_PageDown:
+            if self.scale > 1:
+                self.scale -= 1
+                self.show_map()
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
